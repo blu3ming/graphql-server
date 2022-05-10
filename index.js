27b17d74ee8dad91480773203c6b23033a73e607
LJosueCM <ljosuecm@gmail.com>
2022-05-10 14:31:15 -0500

    Creacion de proyecto e inicializacion de datos

---
+++ index.js
@@ -0,0 +1,23 @@
+const personas = [
+    {
+        name: "Hyojung",
+        phone: "11111111",
+        street: "10",
+        city: "Seoul",
+        id: "OMG-1"
+    },
+    {
+        name: "Seunghee",
+        phone: "22222222",
+        street: "11",
+        city: "Seoul",
+        id: "OMG-2"
+    },
+    {
+        name: "Yooa",
+        phone: "33333333",
+        street: "12",
+        city: "Seoul",
+        id: "OMG-3"
+    }
+]
